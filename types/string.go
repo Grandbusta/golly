dfc9e097c41b5c11b30015807c88f1e6ea89e50f
Grandbusta <bustajay30@gmail.com>
2023-09-13 15:12:22 +0100

    message updated

--- types/string.go
+++ types/string.go
@@ -20,34 +20,44 @@ type StringRules struct {
 }
 
 var messages = map[string]string{
-	"string.empty":    "{label} is not allowed to be empty",
-	"string.max":      "{label} must be less than or equal to {limit} characters long",
-	"string.min":      "{label} length must be at least {limit} characters long",
-	"string.alphanum": "{label} must only contain alpha-numeric characters",
-	"string.minmax":   "max must me greater than min",
-	"string.length":   "{label} length must be {limit} characters long",
+	"string.empty":     "{label} is not allowed to be empty",
+	"string.email":     "{label} must be a valid email",
+	"string.uppercase": "{label} must only contain uppercase characters",
+	"string.lowercase": "{label} must only contain lowercase characters",
+	"string.max":       "{label} must be less than or equal to {limit} characters long",
+	"string.min":       "{label} length must be at least {limit} characters long",
+	"string.alphanum":  "{label} must only contain alpha-numeric characters",
+	"string.minmax":    "max must me greater than min",
+	"string.length":    "{label} length must be {limit} characters long",
 }
 
 func StringValidate(str string, r *StringRules, label string) error {
-	fmt.Println(r.Required, len(str))
+	// check if required is present
 	if r.Required {
 		if len(str) <= 0 {
 			return common.Error(messages["string.empty"], fmt.Sprintf("\"%v\"", label))
 		}
 	}
+
+	// min must be greater than max
 	if r.Max != 0 && r.Max < r.Min {
 		return common.Error(messages["string.minmax"])
 	}
+
+	// only execute if min is not 0
 	if r.Min != 0 {
 		if len(str) < r.Min {
 			return common.Error(messages["string.min"], fmt.Sprintf("\"%v\"", label), strconv.Itoa(r.Min))
 		}
 	}
+
+	// only execute if max is not 0
 	if r.Max != 0 {
 		if len(str) > r.Max {
 			return common.Error(messages["string.max"], fmt.Sprintf("\"%v\"", label), strconv.Itoa(r.Max))
 		}
 	}
+
 	if r.Length != 0 && len(str) != r.Length {
 		return common.Error(messages["string.length"], fmt.Sprintf("\"%v\"", label), strconv.Itoa(r.Length))
 	}
